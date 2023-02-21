92375f0d805eb388fc2184f007ca50522d74c387
ReveraK <katya-revera@yandex.ru>
2023-02-21 18:33:02 +0300

    добавлено несколько функций

--- sendbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sendbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -3,6 +3,27 @@ package ru.stqa.pft.sandbox;
 public class MyFirstProgram {
 
     public static void main(String[] args) {
-        System.out.println("Hello, world!!");
+        hello("world");
+        hello("kate");
+
+        double  l = 5;
+        System.out.println("площадь квадрата со стороной " + l + " = " + area(l) );
+
+        double a = 4;
+        double b = 5;
+        System.out.println("площадь прямоугольника со сторонами " + a + " и " + b + " = " + area(a, b));
+
+
+
     }
+    public static void hello(String somebody) {
+        System.out.println("Hello, " + somebody + "!");
+    }
+    public static double area(double len){
+        return len * len;
+    }
+    public static double area(double a, double b){
+        return a * b;
+    }
+
 }
