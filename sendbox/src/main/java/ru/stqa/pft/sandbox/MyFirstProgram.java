ce6f5fa546cd39baef38d35f833940a0cddd989f
ReveraK <katya-revera@yandex.ru>
2023-02-21 11:07:23 +0300

    создан пакет для myfirstprogram

--- sendbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sendbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -1,5 +1,7 @@
-public class MyFirstProgramm {
-	
+package ru.stqa.pft.sandbox;
+
+public class MyFirstProgram {
+
 	public static void main(String[] args) {
 		System.out.println("Hello, world!!");
 	}
