794127d1849e9e0ac1b09c1a0a981e9c2373e429
ReveraK <katya-revera@yandex.ru>
2023-03-05 19:25:02 +0300

    урок 3, попытка 2

--- sendbox/src/test/java/ru/stqa/pft/sendbox/PointTests.java
+++ sendbox/src/test/java/ru/stqa/pft/sendbox/PointTests.java
@@ -6,31 +6,29 @@ import ru.stqa.pft.sandbox.Point;
 
 public class PointTests {
 
-  double res;
-  public PointTests() {
-
-    Point a = new Point(5,7);
-    Point b = new Point(6,8);
-    res = a.distance(b);
-    System.out.println(" расстояние между двумя точками " + res);
-  }
-
   @Test
   public void test1Point() {
-
-    Assert.assertEquals(res, 1);
+    Point a = new Point(2,7);
+    Point b = new Point(6,3);
+    double res = a.distance(b);
+    Assert.assertNotEquals(res, 1);
 
   }
 
   @Test
   public void test2Point() {
-
+    Point a = new Point(5,7);
+    Point b = new Point(6,8);
+    double res = a.distance(b);
     Assert.assertEquals(res, 1.4142135623730951 );
   }
 
   @Test
   public void test3Point() {
-
-    assert res > 0;
+    Point a = new Point(5,5);
+    Point b = new Point(5,6);
+    double res = a.distance(b);
+    Assert.assertEquals(res, 1.0 );
   }
+
 }
