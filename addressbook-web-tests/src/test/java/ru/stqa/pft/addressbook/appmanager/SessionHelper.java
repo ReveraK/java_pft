3effdafdbba5057eec08b91e2ddec40a6959401e
ReveraK <katya-revera@yandex.ru>
2023-03-13 17:39:19 +0300

    в public SessionHelper изменила логирование,
    добавила     wd.findElement(By.name("user")).click();

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/SessionHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/SessionHelper.java
@@ -11,6 +11,7 @@ public class SessionHelper {
     this.wd = wd;
   }
   public void login(String username, String password) {
+    wd.findElement(By.name("user")).click();
     wd.findElement(By.name("user")).clear();
     wd.findElement(By.name("user")).sendKeys(username);
     wd.findElement(By.name("pass")).click();
