ea1bff0cddf46d5dade9fc79aae32bbf6c174900
ReveraK <katya-revera@yandex.ru>
2023-03-13 17:35:59 +0300

    удалила лишние комментарии

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -16,26 +16,14 @@ public class ApplicationManager {
   private SessionHelper sessionHelper;
 
   public void init() {
-    //new GroupsHelper();
     wd = new ChromeDriver();
     wd.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
-    //wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-    //js = (JavascriptExecutor) wd;
     wd.get("http://localhost/addressbook/group.php");
     groupsHelper = new GroupsHelper(wd);
     navigationHelper = new NavigationHelper(wd);
     sessionHelper = new SessionHelper(wd);
     sessionHelper.login("admin", "secret");
   }
-
-  //public void login(String username, String password) {
-   // wd.findElement(By.name("user")).clear();
-   // wd.findElement(By.name("user")).sendKeys(username);
-  //  wd.findElement(By.name("pass")).click();
-   // wd.findElement(By.name("pass")).clear();
-   // wd.findElement(By.name("pass")).sendKeys(password);
-   // wd.findElement(By.xpath("//input[@value='Login']")).click();
-  //}
 
   public void stop() {
     wd.findElement(By.linkText("Logout")).click();
