c26fc9792148b272c5d6c812286d7ac490c069c7
ReveraK <katya-revera@yandex.ru>
2023-03-15 20:57:23 +0300

    Урок 5
    переделали тест AddNewContactTestCase под двухуровневую архитектуру.  переписали ContactHelper

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
@@ -13,53 +13,33 @@ public class ContactHelper extends HelperBase {
     click(By.name("submit"));
   }
   public void creationContact(ContactData contactData) {
-    wd.findElement(By.name("firstname")).click();
-    wd.findElement(By.name("firstname")).clear();
-    wd.findElement(By.name("firstname")).sendKeys(contactData.firstname());
-    wd.findElement(By.name("middlename")).click();
-    wd.findElement(By.name("middlename")).clear();
-    wd.findElement(By.name("middlename")).sendKeys(contactData.middlename());
-    wd.findElement(By.name("lastname")).click();
-    wd.findElement(By.name("lastname")).clear();
-    wd.findElement(By.name("lastname")).sendKeys(contactData.lastname());
-    wd.findElement(By.name("nickname")).click();
-    wd.findElement(By.name("nickname")).clear();
-    wd.findElement(By.name("nickname")).sendKeys(contactData.nickname());
-    wd.findElement(By.name("title")).click();
-    wd.findElement(By.name("title")).clear();
-    wd.findElement(By.name("title")).sendKeys(contactData.title());
-    wd.findElement(By.name("company")).click();
-    wd.findElement(By.name("company")).clear();
-    wd.findElement(By.name("company")).sendKeys(contactData.company());
-    wd.findElement(By.name("address")).click();
-    wd.findElement(By.name("address")).clear();
-    wd.findElement(By.name("address")).sendKeys(contactData.address());
-    wd.findElement(By.name("home")).click();
-    wd.findElement(By.name("home")).clear();
-    wd.findElement(By.name("home")).sendKeys(contactData.telhome());
-    wd.findElement(By.name("mobile")).click();
-    wd.findElement(By.name("mobile")).clear();
-    wd.findElement(By.name("mobile")).sendKeys(contactData.telmobile());
-    wd.findElement(By.name("work")).click();
-    wd.findElement(By.name("work")).clear();
-    wd.findElement(By.name("work")).sendKeys(contactData.telwork());
-    wd.findElement(By.name("fax")).click();
-    wd.findElement(By.name("fax")).clear();
-    wd.findElement(By.name("fax")).sendKeys(contactData.fax());
-    wd.findElement(By.name("email")).click();
-    wd.findElement(By.name("email")).clear();
-    wd.findElement(By.name("email")).sendKeys(contactData.email());
-    wd.findElement(By.name("bday")).click();
-    new Select(wd.findElement(By.name("bday"))).selectByVisibleText(contactData.bday());
-    wd.findElement(By.xpath("//option[@value='"+ contactData.bday() +"']")).click();
-    wd.findElement(By.name("bmonth")).click();
-    new Select(wd.findElement(By.name("bmonth"))).selectByVisibleText(contactData.bmonth());
-    wd.findElement(By.xpath("//option[@value='" + contactData.bmonth() +"']")).click();
-    wd.findElement(By.name("byear")).click();
-    wd.findElement(By.name("byear")).clear();
-    wd.findElement(By.name("byear")).sendKeys(contactData.byear());
-    wd.findElement(By.name("new_group")).click();
-    new Select(wd.findElement(By.name("new_group"))).selectByVisibleText(contactData.group());
-    wd.findElement(By.xpath("//div[@id='content']/form/select[5]/option[2]")).click();
+
+    type(By.name("firstname"), contactData.firstname());
+    type(By.name("middlename"), contactData.middlename());
+    type(By.name("lastname"), contactData.lastname());
+    type(By.name("nickname"), contactData.nickname());
+    type(By.name("title"), contactData.title());
+    type(By.name("company"), contactData.company());
+    type(By.name("address"), contactData.address());
+    type(By.name("home"), contactData.telhome());
+    type(By.name("mobile"), contactData.telmobile());
+    type(By.name("work"), contactData.telwork());
+    type(By.name("fax"), contactData.fax());
+    type(By.name("email"), contactData.email());
+    selectDate("bday", contactData.bday());
+    selectDate("bmonth", contactData.bmonth());
+    type(By.name("byear"), contactData.byear());
+    selectGroup("new_group", contactData.group());
+  }
+  private void selectComboBox(String name, String value, String xpath){
+    click(By.name(name));
+    new Select(wd.findElement(By.name(name))).selectByVisibleText(value);
+    click(By.xpath(xpath));
+  }
+  public void selectDate(String name, String value){
+    selectComboBox(name, value, "//option[@value='"+ value +"']");
+  }
+  public void selectGroup(String name, String value){
+    selectComboBox(name, value, "//div[@id='content']/form/select[5]/option[2]");
   }
 }
