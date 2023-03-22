b129491a425a467635a46183c538af0b0ce112d6
ReveraK <katya-revera@yandex.ru>
2023-03-22 21:40:57 +0300

    урок 7

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -5,7 +5,10 @@ import org.openqa.selenium.NoAlertPresentException;
 //import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.Browser;
 
 import java.time.Duration;
 import java.util.concurrent.TimeUnit;
@@ -17,11 +20,28 @@ public class ApplicationManager {
   private GroupsHelper groupsHelper;
   private SessionHelper sessionHelper;
   private ContactHelper contactHelper;
+  private String browser;
+
+  public ApplicationManager(String browser) {
+
+    this.browser = browser;
+  }
 
   public void init() {
+    if (browser.equals(Browser.FIREFOX.browserName())) {
+      wd = new FirefoxDriver();
+    } else if (browser.equals(Browser.CHROME.browserName())) {
+      System.setProperty("chromedriver.chrome.driver", "path_here");
+      ChromeOptions options = new ChromeOptions();
+      options.addArguments("--remote-allow-origins=*");
+      wd = new ChromeDriver(options);
+      //wd = new ChromeDriver();
+    } else if (browser.equals(Browser.IE.browserName())) {
+      wd = new InternetExplorerDriver();
+    }
     //wd = new ChromeDriver();
     //wd.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
-    wd = new FirefoxDriver();
+
     wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
     wd.get("http://localhost/addressbook/group.php");
     groupsHelper = new GroupsHelper(wd);
