f50b8faf34a03a9a6144296bee489868213a37aa
ReveraK <katya-revera@yandex.ru>
2023-03-30 17:11:56 +0300

    4.6_1

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
@@ -1,4 +1,49 @@
 package ru.stqa.pft.addressbook.model;
 
-public record GroupData(String name, String heder, String footer) {
+import java.util.Objects;
+
+public final class GroupData {
+  private final String name;
+  private final String heder;
+  private final String footer;
+
+  public GroupData(String name, String heder, String footer) {
+    this.name = name;
+    this.heder = heder;
+    this.footer = footer;
+  }
+
+  public String name() {
+    return name;
+  }
+
+  public String heder() {
+    return heder;
+  }
+
+  public String footer() {
+    return footer;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (obj == this) return true;
+    if (obj == null || obj.getClass() != this.getClass()) return false;
+    var that = (GroupData) obj;
+    return Objects.equals(this.name, that.name) &&
+            Objects.equals(this.heder, that.heder) &&
+            Objects.equals(this.footer, that.footer);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(name, heder, footer);
+  }
+
+  @Override
+  public String toString() {
+    return "GroupData{" +
+            "name='" + name + '\'' +
+            '}';
+  }
 }
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupDeletionTests.java
@@ -21,5 +21,10 @@ public class GroupDeletionTests extends TestBase {
     app.getGroupsHelper().returnToGoPage();
     List<GroupData> after = app.getGroupsHelper().getGroupList();
     Assert.assertEquals(after.size(), before.size() - 1);
+
+    before.remove(before.size() - 1);
+    for (int i =0; i < after.size(); i++) {
+      Assert.assertEquals(before.get(i), after.get(i));
+    }
   }
 }
