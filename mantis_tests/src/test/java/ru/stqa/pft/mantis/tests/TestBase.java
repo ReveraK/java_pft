282df6855325806dfea796610705d05291e4a00c
ReveraK <katya-revera@yandex.ru>
2023-04-13 22:08:15 +0300

    8.2
    добавлен модуль для mantis

---
+++ mantis_tests/src/test/java/ru/stqa/pft/mantis/tests/TestBase.java
@@ -0,0 +1,23 @@
+package ru.stqa.pft.mantis.tests;
+
+import org.openqa.selenium.remote.Browser;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import ru.staq.pft.mantis.appmanager.ApplicationManager;
+
+
+public class TestBase {
+
+  protected static final ApplicationManager app
+          = new ApplicationManager(System.getProperty("browser", Browser.CHROME.browserName()));
+
+  @BeforeSuite(alwaysRun = true)
+  public void setUp() throws Exception {
+    app.init();
+  }
+
+  @AfterSuite(alwaysRun = true)
+  public void tearDown() throws Exception {
+    app.stop();
+  }
+}
